1e85228d52c14576c5d15b947093625f967a258e
Shuvadarshan Bhual <sbhual1998@gmail.com>
2021-06-13 23:29:01 +0200

    Defined the increment function in JS

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-let firstBatch = 5;
-let secondBatch = 7;
+// Adding some functionality to the button
+// Initialize the count as 0
+let count = 0;
 
-let count = firstBatch + secondBatch;
-console.log(count); 
+// Listen for clicks on the increment button (The 'onclick' event listener in html)
+
+// Increment the count variable when the button is clicked (and log it out)
+function increment() {
+    count+=1;
+    
+    // change the count-el in the HTML to reflect the new count
+    document.getElementById("count-el").innerText = count; // "count-el" represents the argument of the function here
+}
+
 
